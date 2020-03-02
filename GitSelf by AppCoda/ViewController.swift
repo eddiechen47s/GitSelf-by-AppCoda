c1752566fc9dd645ca62d974fd63dc1c4f28848e
Eddie <eddiechen47s@gmail.com>
2020-03-02 18:20:57 +0800

    add print("new data")

--- GitSelf by AppCoda/ViewController.swift
+++ GitSelf by AppCoda/ViewController.swift
@@ -23,6 +23,8 @@ class ViewController: UIViewController {
         print(reverse(message) + "hello")
         
         lable.text = reverse(message)
+        
+        print("new data")
     }
 
     
