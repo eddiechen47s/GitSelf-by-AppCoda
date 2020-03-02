791ee15ad45436f409e8ab1f917081765884bd45
Eddie <eddiechen47s@gmail.com>
2020-03-02 17:13:01 +0800

    Initial Commit

---
+++ GitSelf by AppCoda/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitSelf by AppCoda
+//
+//  Created by ktrade on 2020/3/2.
+//  Copyright © 2020 Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
